8ac12ab85be6438da1967b5a1d7548fc705b8027
Harsh Kumar <9u.harsh@gmail.com>
2022-06-26 08:09:52 +0530

    uses ComponentDidMount to fetch api also uses promises

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,7 @@ export class NewsItem extends Component {
     return (
       <div>
         <div className="card" style={{width: "18rem"}}>
-            <img src={imageUrl} className="card-img-top" alt="..." />
+            <img src={!imageUrl ? "https://thegadgetflow.com/wp-content/uploads/2020/09/Jabra-Elite-85t-True-Wireless-Earbuds-01.jpg" : imageUrl} className="card-img-top" alt="..." />
             <div className="card-body">
                 <h5 className="card-title">{title}..</h5>
                 <p className="card-text">{description}..</p>
